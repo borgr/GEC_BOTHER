f1ab05d7adb282457c70dfc5145d8e32d4c08b0b
Ofir Shifman <ofirshifman@cs.huji.ac.il>
2018-11-21 16:25:17 +0200

    added the parser

--- NUCLE/my_NUCLE_parser/parser.py
+++ NUCLE/my_NUCLE_parser/parser.py
@@ -6,28 +6,58 @@ import random;
 ATRIBUTES = "A"
 SENTENCE = "S"
 
+# sentence is a tuple as follows: (the nucle original sentence, list of mistakes)
+
+LINE_INX = 0
+SENTANCE_INX = 1
+MISTAKES_INX = 2
+
+# the list of mistakes and cerrections is as follows: [mistake starting index, mistake end index, mistake type, correction]
+NUM_OF_MISTAKE_ATTRIBUTES = 4
+ATTRIBUTES_INX = 1
 
 
+def get_random_inx(lines):
+    while True:
+        i = random.randint(0, len(lines))
+        line = lines[i].split()
+        if len(line) > 4 and line[0] == SENTENCE: #it's a valid sentence
+            break
+    return i
 
+def parse_mistake(line):
+    if line.startswith(ATRIBUTES):
+        mistake = line.replace('|', ' ').split()
+        return mistake[ATTRIBUTES_INX:ATTRIBUTES_INX+NUM_OF_MISTAKE_ATTRIBUTES] #1:5 in NUCLE as it is
+    return False
 
-
-
-def get_sentance(lines):
-    line = "X" #some fale value for the do-while
-    while line.startswith(SENTENCE):
-        i = random.randint(0,lines.len)
-        line = lines[i]
-    return line
-
-def parse_sentance():
+def parse_sentence(lines, i):
+    line = lines[i][2:]
+    sentence = (i, line, [])
+    while True:
+        i += 1
+        cur_line = lines[i]
+        mistake = parse_mistake(cur_line)
+        if mistake: #True if parsed a mistake
+            sentence[MISTAKES_INX].append(mistake)
+        else: #last sentence to be parsed was not a mistake - end of mistakes for this sentence
+            break
+    return sentence
 
 
 def main():
-    file_name = sys.argv[1]  # NUCLE db address
-    lines = open(file_name).read().splitlines();
-    for i
+    file_name = "/cs/usr/ofirshifman/safe/GEC_ME_PROJECT/NUCLE/release3.2/data/conll14st-preprocessed.m2"  # NUCLE db address
+    lines = open(file_name).read().splitlines()
+    for k in range(20):
+        parsed_sentence = (parse_sentence(lines, get_random_inx(lines)))
+        print ("sentence number " + str(k+1) + " is:\n" + parsed_sentence[SENTANCE_INX])
+        print("mistakes for sentence number " + str(k+1) + " are:")
+        for mistake in parsed_sentence[MISTAKES_INX]:
+            print(mistake)
+        print()
+
 
 
 
 if __name__ == '__main__':
-    main();
+    main()
