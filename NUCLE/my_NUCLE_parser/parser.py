121aeda4b601ddffe8014527fac5239638026c37
Ofir Shifman <ofirshifman@cs.huji.ac.il>
2018-11-26 18:12:53 +0200

    add -a

--- NUCLE/my_NUCLE_parser/parser.py
+++ NUCLE/my_NUCLE_parser/parser.py
@@ -1,6 +1,7 @@
 # a NUCLE parser for ME (mistake evaluation) project.
 
 import random;
+import regex
 
 ATRIBUTES = "A"
 SENTENCE = "S"
@@ -20,14 +21,16 @@ def get_random_inx(lines):
     while True:
         i = random.randint(0, len(lines))
         line = lines[i].split()
-        if len(line) > 4 and line[0] == SENTENCE: #it's a valid sentence
+        if len(line) > 4 and line[0] == SENTENCE and (
+                "http" not in line):  # it's a valid sentence - more then 4 words, and doesn't include http address
             break
     return i
+
 
 def parse_mistake(line):
     if line.startswith(ATRIBUTES):
         mistake = line.replace('|', ' ').split()
-        mistake = mistake[ATTRIBUTES_INX:ATTRIBUTES_INX+NUM_OF_MISTAKE_ATTRIBUTES] #1:5 in NUCLE as it is
+        mistake = mistake[ATTRIBUTES_INX:ATTRIBUTES_INX + NUM_OF_MISTAKE_ATTRIBUTES]  # 1:5 in NUCLE as it is
         mistake[0] = int(mistake[0])
         mistake[1] = int(mistake[1])
         if mistake[3] == 'REQUIRED':
@@ -35,16 +38,17 @@ def parse_mistake(line):
         return mistake
     return False
 
+
 def parse_sentence(lines, i):
     line = lines[i][2:]
-    sentence = (i+1, line, [])
+    sentence = (i + 1, line, [])
     while True:
         i += 1
         cur_line = lines[i]
         mistake = parse_mistake(cur_line)
-        if mistake: #True if parsed a mistake
+        if mistake:  # True if parsed a mistake
             sentence[MISTAKES_INX].append(mistake)
-        else: #last sentence to be parsed was not a mistake - end of mistakes for this sentence
+        else:  # last sentence to be parsed was not a mistake - end of mistakes for this sentence
             break
     return sentence
 
@@ -52,15 +56,25 @@ def parse_sentence(lines, i):
 def main():
     file_name = "/cs/usr/ofirshifman/safe/GEC_ME_PROJECT/NUCLE/release3.2/data/conll14st-preprocessed.m2"  # NUCLE db address
     lines = open(file_name).read().splitlines()
+    out_file = open("/cs/usr/ofirshifman/safe/GEC_ME_PROJECT/NUCLE/my_NUCLE_parser/out_file.txt", "w")
+
+
     for k in range(20):
         parsed_sentence = (parse_sentence(lines, get_random_inx(lines)))
-        print ("sentence number " + str(k+1) + " is originaly in line " + str(parsed_sentence[LINE_INX]) + " and is:\n" + parsed_sentence[SENTANCE_INX])
-        print("mistakes for sentence number " + str(k+1) + " are:")
+        line = parsed_sentence[SENTANCE_INX]
+        line = line.replace(' ,', ',')  # just to make the sentence look a bit nicer
+        line = line.replace(' .', '.')
+        line = line.replace('( ', '(')
+        line = line.replace(' )', ')')
+        line = line.replace(' \'', '\'')
+        out_file.write("sentence number " + str(k + 1) + " is originaly in line " + str(
+            parsed_sentence[LINE_INX]) + " and is:\n" + line)
+        out_file.write("\nmistakes for sentence number " + str(k + 1) + " are:\n")
         for mistake in parsed_sentence[MISTAKES_INX]:
-            print(mistake)
-        print()
+            out_file.write(str(mistake)+ "\n")
+        out_file.write('\n')
 
-
+    out_file.close()
 
 
 if __name__ == '__main__':
